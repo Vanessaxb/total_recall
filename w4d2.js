ec8a2db501d40c12d47e19bc0ddb2854fa2ec7c5
Vanessa <vanessambeattie@gmail.com>
2023-07-02 22:08:19 -0500

    First commit

---
+++ w4d2.js
@@ -0,0 +1,21 @@
+/*
+A. Q + A
+How do we assign a value to a variable?
+How do we change the value of a variable?
+How do we assign an existing variable to a new variable?
+Remind me, what are declare, assign, and define?
+What is pseudocoding and why should you do it?
+What percentage of time should be spent thinking 
+about how you're going to solve a 
+problem vs actually typing in code to solve it?
+*/
+
+//*B. Strings
+let firstVariable = "Hello World"
+firstVariable = 15
+let secondVariable = firstVariable;
+secondVariable = 'Happy';
+console.log(secondVariable);
+console.log(firstVariable);
+let yourName = 'Vanessa';
+console.log(`Hello, my name is ${yourName}`);
