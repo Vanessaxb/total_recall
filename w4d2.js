848faa507a717a12a33988e7b7a0b9d4bb67637d
Vanessa <vanessambeattie@gmail.com>
2023-07-03 10:49:40 -0500

    Farm complete

--- w4d2.js
+++ w4d2.js
@@ -19,3 +19,29 @@ console.log(secondVariable);
 console.log(firstVariable);
 let yourName = 'Vanessa';
 console.log(`Hello, my name is ${yourName}`);
+
+//*C. Booleans
+const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log('Name' === 'Name');
+  // FOR THE NEXT TWO, USE ONLY && OR ||
+  console.log(true || false);
+  console.log(false && false && false && false && false || true);
+  console.log(false == false) //*I dont understand this
+  console.log(e === 'Kevin');
+  console.log(a + b == c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+  console.log(a * a == d); // note: the answer is a simple arithmetic equation, not something "weird"
+  console.log(48 == '48');
+
+  //* D. The farm
+  const animal= "co"
+  if (animal === "cow") {console.log('moooo');
+    } else {
+      console.log("Hey! You're not a cow.");  
+    }
