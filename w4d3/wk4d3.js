f10789b1b36165ebc5b95b38e51e58de1a691780
Vanessa <vanessambeattie@gmail.com>
2023-07-06 17:01:09 -0500

    Get Even answered

--- w4d3/wk4d3.js
+++ w4d3/wk4d3.js
@@ -5,3 +5,10 @@
 for (let number = 1; number <= 20; number++) {
     console.log(number);
 }
+
+//*Get Even - Write a for loop that will log only the even numbers in 0 through 200.
+for (i = 0; i <= 200; i++) {
+    if (i % 2 == 0){
+    console.log(i);
+}
+}
