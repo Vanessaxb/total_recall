18d35846e63ca6a73786facf9fb1356f75c7c25a
Vanessa <vanessambeattie@gmail.com>
2023-07-06 16:53:47 -0500

    Easy Going answered

---
+++ w4d3/wk4d3.js
@@ -0,0 +1,7 @@
+//maybe copy here previous examples
+
+//*Part II: JavaScript Reps
+//* Easy Going - Write a for loop that will log the numbers 1 through 20.
+for (let number = 1; number <= 20; number++) {
+    console.log(number);
+}
