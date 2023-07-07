cd06d9d3f7056eeaaa861be1c38c7186e537d774
Vanessa <vanessambeattie@gmail.com>
2023-07-07 10:24:38 -0500

    Find the Median answered

--- w4d3/wk4d3.js
+++ w4d3/wk4d3.js
@@ -18,8 +18,7 @@ for (rep = 1; rep <= 20; rep++) {
         if (rep % 2 == 0) {
         const kittyTalk = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
         let meow = Math.round(Math.random()* 2);
-       
-        
+            
         console.log(kittyTalk[meow]);
     } else {
         console.log('Love me, pet me! HSSSSSS!');
@@ -27,3 +26,12 @@ for (rep = 1; rep <= 20; rep++) {
 }
 // For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", 
 //or "...why does the red dot always get away..." at random.
+
+//* Find the Median
+//ind the median number in the following numsarray, then console.log that number.
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+nums.sort()
+console.log(nums[Math.floor((nums.length) / 2)]);
+
+
+
