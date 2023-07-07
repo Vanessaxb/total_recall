d50d3d6c5263170475dc426b148e90a6c5ca0668
Vanessa <vanessambeattie@gmail.com>
2023-07-07 09:51:54 -0500

    Excited Kittens answered

--- w4d3/wk4d3.js
+++ w4d3/wk4d3.js
@@ -12,3 +12,18 @@ for (i = 0; i <= 200; i++) {
     console.log(i);
 }
 }
+//*Excited Kitten
+// Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+for (rep = 1; rep <= 20; rep++) {
+        if (rep % 2 == 0) {
+        const kittyTalk = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+        let meow = Math.round(Math.random()* 2);
+       
+        
+        console.log(kittyTalk[meow]);
+    } else {
+        console.log('Love me, pet me! HSSSSSS!');
+    }
+}
+// For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", 
+//or "...why does the red dot always get away..." at random.
